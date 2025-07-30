f6cac7f033350db0cdf0debbf57cba8bf05af569
FriendlyDemon <henriquemichel@live.com>
2025-07-30 14:58:16 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/projeto_final_front/',
-  plugins: [react()],
-})
